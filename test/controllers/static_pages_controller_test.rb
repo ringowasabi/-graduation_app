68c103c4558a4c1b1f763e3ebffb266428a38958
kaori <kaori.nagumo1122@gmail.com>
2026-06-04 21:54:43 +0900

    Address review feedback for navigation and deletion

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -20,6 +20,10 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
 
     assert_response :success
     assert_select "a[href=?]", transportation_memos_path, text: "交通費メモ一覧"
+    assert_select "a[href=?]", new_transportation_memo_path, text: "交通費メモを登録する"
+    assert_select "a[href=?]", transportation_memos_path, text: "交通費メモ一覧を見る"
+    assert_select "a[href=?]", signup_path, text: "交通費メモをはじめる", count: 0
+    assert_select "a[href=?]", login_path, text: "ログインする", count: 0
     assert_select "form[action=?][method=?]", logout_path, "post"
     assert_select "input[name=?][value=?]", "_method", "delete"
   end
